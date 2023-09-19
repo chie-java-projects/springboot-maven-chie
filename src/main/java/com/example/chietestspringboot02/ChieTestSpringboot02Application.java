ab1abe1b8f33000dc6def05237fe21c2f4b92ff4
cm-cmd <chaimin419@163.com>
2023-09-19 14:51:38 +0800

    [cm第一次提交]

---
+++ src/main/java/com/example/chietestspringboot02/ChieTestSpringboot02Application.java
@@ -0,0 +1,13 @@
+package com.example.chietestspringboot02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChieTestSpringboot02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChieTestSpringboot02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chietestspringboot02/ChieTestSpringboot02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chietestspringboot02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChieTestSpringboot02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
